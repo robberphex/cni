1e19f9712b83c0780f510645765406286487393f
luyanbo <robert.lyb@alibaba-inc.com>
2022-02-10 15:44:30 +0800

    reuse v1, only ListNetworkNamespace method

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-protoc -I pkg/types/v2/ \
+protoc -I pkg/types/v1/ \
     -I${GOPATH}/src/github.com/protocolbuffers/protobuf/src/ \
-    --go_out=pkg/types/v2/ --go_opt=paths=source_relative \
-    --go-grpc_out=pkg/types/v2/ --go-grpc_opt=paths=source_relative \
-    pkg/types/v2/cni.proto
+    --go_out=pkg/types/v1/ --go_opt=paths=source_relative \
+    --go-grpc_out=pkg/types/v1/ --go-grpc_opt=paths=source_relative \
+    pkg/types/v1/cni.proto
--- go.mod
+++ go.mod
@@ -5,4 +5,6 @@ go 1.14
 require (
 	github.com/onsi/ginkgo v1.13.0
 	github.com/onsi/gomega v1.10.1
+	google.golang.org/grpc v1.44.0
+	google.golang.org/protobuf v1.25.0
 )
